0b2b0891857b90c8743e3142070cac6d5dfa0581
zinebnadak <zinebnadak04@outlook.com>
2025-10-20 09:30:00 +0300

    20.10

--- #ITQuestions2.py
+++ #ITQuestions2.py
@@ -1,1 +1,8 @@
-#Databases
+#DATABASES
+
+
+#Without databases, none of the IT systems we rely on could function reliably and efficiently.
+#In this course, we cover the fundamental principles of databases in both theory and practice.
+#We focus primarily on relational databases and the SQL query language, but we also get acquainted with document databases.
+
+
